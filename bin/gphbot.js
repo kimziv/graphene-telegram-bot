086219234a5cf127371e406069026a537e59cee5
Cheng Liang <chengliang@meishubao.com>
2017-05-18 08:24:14 +0800

    fix

--- bin/gphbot.js
+++ bin/gphbot.js
@@ -98,7 +98,7 @@ var main = function(){
   for (var i = 0; i < objList.length; i++) {
     obj= objList[i]
     if (obj.MsgType === 1) {
-     console.log("####getMesg:"+JSON.stringfy(obj));
+     console.log("####getMesg:"+JSON.stringify(obj));
       // let obj = JSON.parse(res.text)
       // test.sendMesg({
       //   content: obj.text,
